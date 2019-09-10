6a834fa57ecbf2bda7b4b926d6ed36c69c08ff8d
Kevin Suen <kevinsuen11@gmail.com>
2019-09-09 22:16:49 -0400

    Implement testing for reducer

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,42 +1,14 @@
 import { useEffect, useReducer } from 'react';
 import Axios from 'axios';
 
-const useApplicationData = () => {
-  const SET_DAY = 'SET_DAY';
-  const SET_APPLICATION_DATA = 'SET_APPLICATION_DATA';
-  const SET_INTERVIEW = 'SET_INTERVIEW';
-  const SET_SPOTS = "SET_SPOTS";
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW,
+  SET_SPOTS
+} from "reducers/application";
 
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case SET_DAY:
-        return {
-          ...state,
-          day: action.value
-        };
-      case SET_APPLICATION_DATA:
-        return {
-          ...state,
-          days: action.value[0].data,
-          appointments: action.value[1].data,
-          interviewers: action.value[2].data
-        };
-      case SET_INTERVIEW:
-        return {
-          ...state,
-          appointments: action.value
-        };
-      case SET_SPOTS:
-        return {
-          ...state,
-          days: action.value
-        }
-      default:
-        throw new Error(
-          `Tried to reduce with unsupported action type: ${action.type}`
-        );
-    }
-  };
+const useApplicationData = () => {
 
   const getWeekDay = date => {
     const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
